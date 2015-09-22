10598ee33069dcce6e3af227c39c686df7f8edfe
Tristan Himmelman <tristanhimmelman@gmail.com>
2015-09-22 10:48:23 -0700

    added a framework to project

---
+++ HidingNavigationBarSample/HidingNavigationBar/HidingNavigationBar.h
@@ -0,0 +1,19 @@
+//
+//  HidingNavigationBar.h
+//  HidingNavigationBar
+//
+//  Created by Tristan Himmelman on 2015-09-22.
+//  Copyright © 2015 Tristan Himmelman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HidingNavigationBar.
+FOUNDATION_EXPORT double HidingNavigationBarVersionNumber;
+
+//! Project version string for HidingNavigationBar.
+FOUNDATION_EXPORT const unsigned char HidingNavigationBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HidingNavigationBar/PublicHeader.h>
+
+
